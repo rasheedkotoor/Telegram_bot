9da0106cf16fa693ec7ca27038ad43ceb67a913a
rasheedkotoor <rasheedkmkotoor@gmail.com>
2021-07-27 19:29:12 +0530

    static dir

--- home/helpers.py
+++ home/helpers.py
@@ -4,8 +4,6 @@ import random
 def get_response(message):
     """ Returns random response according to message received """
 
-    # payload = requests.get(
-    #     f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={message}&date={current_date}')
     jokes = {
      'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
                 """Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
